89af26f748c23870bceddd14ba0ba613ad506023
Victor <2020mireles2020@gmail.com>
2026-08-31 10:54:27 -0600

    actualizacion de funciones

--- calculadora.py
+++ calculadora.py
@@ -2,30 +2,16 @@ def indicaciones():
     print ("Esta es la prueba de una calculadora de 'basica'")
     print ("los unicos signos permitidos para el uso de esta calculadora son: /n")
 
-#Las siguientes funciones seran las operaciones que tendran disponibles la calculadora
-"""
-def suma(a, b):
-    #Esta funcion nos permite realizar una suma
-    return a + b
-def resta(a, b):
-    #Esta funcion nos permite realizar una resta
-    return a - b
-def multiplicacion(a, b):
-    #Esta funcion nos permite realizar una multiplicacion
-    return a * b
-def division(a, b):
-    #Esta funcion nos permite realizar una division
-    if b == 0:
-        print ("No se puede dividir entre cero") 
-    return a / b 
-def eleccion_signo():
-    operaciones_validas ={"+", "*", "-", "/", "="}
-""" 
 def operacion (a, b, signo):
-    if signo == "+": resultado = a + b
-    if signo == "-": resultado = a - b
-    if signo == "*": resultado = a * b
-    if signo == "/": resultado = a / b
+    resultado = 0
+    if signo == "+": 
+        resultado = a + b
+    if signo == "-": 
+        resultado = a - b
+    if signo == "*": 
+        resultado = a * b
+    if signo == "/": 
+        resultado = a / b
     print (resultado)
     return resultado 
 
@@ -33,7 +19,7 @@ def eleccion_operacion ():
     operaciones_validas ={"+", "*", "-", "/", "="}
     signo = input ("ingresa un signo aritmetico de '+', '-', '*', '/' ")
     if signo in operaciones_validas:
-        return signo 
+        return signo, operaciones_validas
     else:
         print ("los unicos signos aritmeticos que puedes usar son '+', '-', '*', '/' ")
 
@@ -48,59 +34,46 @@ def ingreso_de_numeros():
             print("Solo puedes ingresar numeros")
     
 
+def segunda_operacion_fun ():
+    segunda_operacion = input("esperando")
+    try:
+        numero = float (segunda_operacion)
+        validar_numero = True
 
+    except ValueError:
+        validar_numero = False
+
+    if validar_numero == True:
+        print ("es un numero")
+        return numero 
+    else:
+        signo = str(segunda_operacion)
+        return signo
+    
 
 def motor_de_operaciones():
     valor1 = ingreso_de_numeros()
-    signo = eleccion_operacion ()
+    signo, signo_segunda_suma = eleccion_operacion()
     valor2 = ingreso_de_numeros()
     proceso = operacion(valor1, valor2, signo)
     resultado = proceso
     print (f"este es el valor que volveremos a usar {resultado}")
+    segunda_operacion_var = segunda_operacion_fun()
+    print (type(segunda_operacion_var))
+    if isinstance (segunda_operacion_var, float):
+        print ("la segunda fue considerada como un numero")
+        motor_de_operaciones()
+    if isinstance (segunda_operacion_var, str):
+        valor2 = ingreso_de_numeros()
+        segunda_operacion = operacion(resultado, valor2, segunda_operacion_var)
+        print (segunda_operacion)
+    else:
+        print ("para poder continuar con las operaciones debe de ser un signo o un numero")
+
+
+
 
 
 
 
 motor_de_operaciones()
-
-"""
-        try:
-            var1 = int(input("ingrese el primer valor"))
-            signo = input ("ingresa un signo aritmetico valido")
-            if signo in operaciones_validas:
-                var2 = int(input("ingresa el segundo numero"))
-                if signo == "+":
-                    resultado = suma(var1, var2)
-                    print (resultado)
-                if signo == "-":
-                    resultado = resta(var1, var2)
-                    print (resultado)
-                if signo == "*":
-                    resultado = multiplicacion(var1, var2)
-                    print (resultado)
-                while True: 
-                    if segunda_operacion == "esc":
-                        print ("el sistema finalizara")
-                        break 
-                    if signo in operaciones_validas:
-                        var2 = int(input("ingresa el segundo numero"))
-                        if signo == "+":
-                            resultado = suma(var1, var2)
-                            print (resultado)
-                        if signo == "-":
-                            resultado = resta(var1, var2)
-                            print (resultado)
-                        if signo == "*":
-                            resultado = multiplicacion(var1, var2)
-                            print (resultado)
-
-            else:     
-                print ("la operacion no esta hablitada")
-                print ("intentelo nuevamente")
-
-        except ValueError:
-            print("solo se pueden ingresar numeros validos")
-""" 
-
-
-#motor_de_operaciones()
